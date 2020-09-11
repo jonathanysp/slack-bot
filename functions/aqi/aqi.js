a39a6e287c2d24c7e1db216c95b959404c06c126
Jonathan Poon <jonathanysp@gmail.com>
2020-09-11 10:08:20 -0700

    aqi

---
+++ functions/aqi/aqi.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+exports.handler = function(event, context, callback) {
+	fetch('https://www.purpleair.com/json?show=62233')
+  		.then(response => response.json())
+  		.then(data => JSON.parse(data.results[0].Stats).v1)
+		.then(aqi =>
+			callback(null, {
+				statusCode: 200,
+				body: "AQI is: " + aqi
+			}));
+}
