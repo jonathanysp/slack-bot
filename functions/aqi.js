07484ccd4fe64be8428cadf8085a11d8addf1186
Jonathan Poon <jonathanysp@gmail.com>
2020-09-11 09:58:32 -0700

    aqi

--- functions/aqi.js
+++ functions/aqi.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 exports.handler = function(event, context, callback) {
 	fetch('https://www.purpleair.com/json?show=62233')
   		.then(response => response.json())
